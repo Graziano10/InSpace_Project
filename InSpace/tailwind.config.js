dd90b0b742bf73e0255879bda66d97cae81fc1e6
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-23 11:27:35 +0200

    prova

--- InSpace/tailwind.config.js
+++ InSpace/tailwind.config.js
@@ -1,10 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+   /*  container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '100%',
+        xl: '100%',
+      },
+    }, */
+    screens: {
+      
+      'xs': '300px',
+
+      's':'548px',
+      
+
+      'md': '768px',
+      
+
+      
+     
+
+      'xl': '1280px',
+     
+
+      '3xl' : '1900px',
+    },
     extend: {},
   },
   plugins: [],
