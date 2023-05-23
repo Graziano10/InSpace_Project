cb8b2ef1f7afce9c28d14cdb433319cab4cdb7c9
Graziano Corineo <grazianocorineo01@gmail.com>
2023-05-24 00:49:50 +0200

    Creato Haburger Menu - modifiche varie -> Header/Home

--- InSpace/src/pages/components/componentsHeader/Header.jsx
+++ InSpace/src/pages/components/componentsHeader/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
 
   return (
     <div className="h-screen text-white flex items-center relative">
-      <section className="w-full bg-cover bg-center h-full bg-[url('src/assets/universe03.jpg')]">
-        <div className="container mx-auto text-center text-white absolute top-60">
+      <section className="w-full bg-cover bg-center h-full bg-[url('src/pages/components/componentsHeader/assetsHeader/moon-3704285_1920.jpg')]">
+        <div className="container mx-auto text-center  text-white flex flex-col justify-center items-center h-full">
           <h1 className="text-5xl font-medium mb-6">Welcome InSpace</h1>
           <p className="text-xl mb-12">
             Un piccolo passo per l'uomo, un grande passo per l'umanita <br></br> oggi ne
--- InSpace/src/pages/components/componentsHeader/Section01.jsx
+++ InSpace/src/pages/components/componentsHeader/Section01.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Section01 = () => {
   const divStyle02 = {
-    backgroundImage: "url('src/assets/universe04.jpg')",
+    backgroundImage: "url('src/pages/components/componentsHeader/assetsHeader/universe04.jpg')"
   };
 
   return (
-    <div className="h-screen text-white flex items-center justify-center relative bg-slate-950">
+    <div className="h-screen text-white flex items-center justify-center relative bg-[#0E0F14]">
       <section className="w-11/12 h-5/6 bg-cover bg-center rounded-lg" style={divStyle02}>
         <div className="container mx-auto text-center text-white flex flex-col justify-center items-center h-screen">
           <h1 className="text-5xl font-medium mb-6">Universo: la meraviglia oltre</h1>
