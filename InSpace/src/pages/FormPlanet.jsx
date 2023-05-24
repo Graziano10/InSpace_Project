1457af1861a25cef81c75bed68b1b0c0ba51646c
Stefano <maclion@live.com>
2023-05-24 15:41:12 +0200

    add : talwind setup

--- InSpace/src/pages/FormPlanet.jsx
+++ InSpace/src/pages/FormPlanet.jsx
@@ -1,12 +1,10 @@
-
-
 const FormPlanet = () => {
-
-    return(
-        <div>
-            <h1>FormPlanet</h1>
-        </div>
-    );
+  return (
+    <div className="flex bg-red-600 w-10 h-[500px] ring-2">
+        <h1>Text</h1>
+      <h1 className="text-1xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 
 export default FormPlanet;
