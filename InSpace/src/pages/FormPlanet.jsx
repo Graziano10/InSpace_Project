ae79e01dec2b76f221f8fa2668bb7774e98f352a
Stefano <maclion@live.com>
2023-05-25 00:00:39 +0200

    add: form base

--- InSpace/src/pages/FormPlanet.jsx
+++ InSpace/src/pages/FormPlanet.jsx
@@ -1,9 +1,45 @@
-import bg_img from '../assets/formBg.png'
+// import bg_img from '../assets/formBg.png'
 
 const FormPlanet = () => {
   return (
-    <div>
-        <img src={bg_img} alt="" className="w-full h-full"/>
+    <div className="w-screen h-screen flex justify-center items-center bg-form-bg">
+        <div className="w-2/4 h-1/6 border ">
+
+        <h1 className="text-white">Universe is waiting for you</h1>
+
+        <label htmlFor="fullname">firstname</label>
+        <input type="text" id='name' placeholder=" name"/>
+
+        <label htmlFor="lastname">lastname</label>
+        <input type="text" id='lastname' placeholder=" lastname"/>
+
+        <label htmlFor="age">age</label>
+        <input type="number" id='age' placeholder=" age"/>
+
+        <label htmlFor="gender">gender</label>
+        <select name="" id="gender">
+            <option value="" selected disabled hidden>Choose here</option>
+            <option value="male">male</option>
+            <option value="male">female</option>
+        </select>
+
+        <label htmlFor="email">email address</label>
+        <input type="text" placeholder=" email"/>
+
+        <label htmlFor="phone_number">Phone Number</label>
+        <input type="tel" placeholder=" +39"/>
+
+        <label htmlFor="city">city</label>
+        <input type="text" />
+
+        <label htmlFor="state">state</label>
+        <input type="text" />
+
+        <label htmlFor="state">zip code</label>
+        <input type="text" />
+        </div>
+
+        
     </div> 
   );
 };
--- InSpace/tailwind.config.js
+++ InSpace/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'form-bg': "url('../assets/formBg.png')"
+      }
+    },
   },
   plugins: [],
 }
