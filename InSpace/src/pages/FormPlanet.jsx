bc31c5c0c72758ae275456a12d38d2e82d6fc0a9
Stefano <maclion@live.com>
2023-05-26 16:55:45 +0200

    add: form base component from tailwind

--- InSpace/src/pages/FormPlanet.jsx
+++ InSpace/src/pages/FormPlanet.jsx
@@ -1,49 +1,58 @@
 // import bg_img from '../assets/formBg.png'
 
-const FormPlanet = () => {
+export const FormPlanet = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center bg-form-bg bg-cover bg-center">
-        <h1 className="text-white w-3/4 text-center text-5xl mb-10" >The space is waiting for you</h1>
-        <div className="w-2/4 h-4/6 border border-zinc-500 bg-zinc-500/50 rounded-2xl">
+    <div className="max-w-2xl mx-auto bg-white p-16">
 
-
-        <label htmlFor="fullname">firstname</label>
-        <input type="text" id='name' placeholder=" name"/>
-
-        <label htmlFor="lastname">lastname</label>
-        <input type="text" id='lastname' placeholder=" lastname"/>
-
-        <label htmlFor="age">age</label>
-        <input type="number" id='age' placeholder=" age"/>
-
-        <label htmlFor="gender">gender</label>
-        <select name="" id="gender">
-            <option value="" selected disabled hidden>Choose here</option>
-            <option value="male">male</option>
-            <option value="male">female</option>
-        </select>
-
-        <label htmlFor="email">email address</label>
-        <input type="text" placeholder=" email"/>
-
-        <label htmlFor="phone_number">Phone Number</label>
-        <input type="tel" placeholder=" +39"/>
-
-        <label htmlFor="city">city</label>
-        <input type="text" />
-
-        <label htmlFor="state">state</label>
-        <input type="text" />
-
-        <label htmlFor="state">zip code</label>
-        <input type="text" />
-
-        <button className="bg-white rounded p-5  ">SUBMIT</button>
+	<form>
+    <div className="grid gap-6 mb-6 lg:grid-cols-2">
+        <div>
+            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First name</label>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required/>
         </div>
-
-        
+        <div>
+            <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last name</label>
+            <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required/>
+        </div>
+        <div>
+            <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Company</label>
+            <input type="text" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Flowbite" required/>
+        </div>  
+        <div>
+            <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Phone number</label>
+            <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required/>
+        </div>
+        <div>
+            <label htmlFor="website" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Website URL</label>
+            <input type="url" id="website" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="flowbite.com" required/>
+        </div>
+        <div>
+            <label htmlFor="visitors" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Unique visitors (per month)</label>
+            <input type="number" id="visitors" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required/>
+        </div>
+    </div>
+    <div className="mb-6">
+        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email address</label>
+        <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required/>
     </div> 
-  );
+    <div className="mb-6">
+        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
+        <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required/>
+    </div> 
+    <div className="mb-6">
+        <label htmlFor="confirm_password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Confirm password</label>
+        <input type="password" id="confirm_password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required/>
+    </div> 
+    <div className="flex items-start mb-6">
+        <div className="flex items-center h-5">
+        <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required/>
+        </div>
+        <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-400">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
+    </div>
+    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</form>
+</div>
+  )
 };
 
 export default FormPlanet;
