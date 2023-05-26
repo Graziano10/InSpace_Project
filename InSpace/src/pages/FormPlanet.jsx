63c2c0f0fcd61483d028367fc7da3258dba9b80f
Stefano <maclion@live.com>
2023-05-26 23:35:31 +0200

    add: modificato responsive form

--- InSpace/src/pages/FormPlanet.jsx
+++ InSpace/src/pages/FormPlanet.jsx
@@ -2,10 +2,10 @@
 
 export const FormPlanet = () => {
   return (
-    <div className="max-w-2xl mx-auto bg-white p-16">
+    <div className="w-full bg-form-bg bg-cover bg-center min-h-screen items-center md:flex ">
 
-	<form>
-    <div className="grid gap-6 mb-6 lg:grid-cols-2">
+	<form className=" mx-auto bg-zinc-500/50 p-6 rounded ">
+    <div className="grid gap-6 mb-6 md:grid-cols-2  ">
         <div>
             <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First name</label>
             <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required/>
@@ -51,7 +51,8 @@ export const FormPlanet = () => {
     </div>
     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 </form>
-</div>
+    </div>
+
   )
 };
 
