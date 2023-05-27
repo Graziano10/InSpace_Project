0069d2fd7f9ef9823887c09ddfd0fd4142941d6d
Graziano Corineo <grazianocorineo01@gmail.com>
2023-05-27 22:29:29 +0200

    NavBar in stato modifica03

--- InSpace/src/components/bundles/header/Header.jsx
+++ InSpace/src/components/bundles/header/Header.jsx
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 
 const Header = () => {
-
   return (
-    <div className="h-screen text-white flex items-center relative">
-      <section className="w-full bg-cover bg-center h-full bg-[url('/src\assets\assets-Header\moon-3704285_1920.jpg')]">
-        <NavBar/>
-        <div className="container mx-auto text-center  text-white flex flex-col justify-center items-center h-full">
+    <>
+    <div className="h-screen text-white flex items-center relative z-1">
+      
+      <section className="w-full bg-cover bg-center h-full bg-[url('/src\assets\assets-Header\moon-3704285_1920.jpg')] ring">
+      <NavBar/>
+        <div className="container mx-auto text-center text-white flex flex-col justify-center items-center h-full z-20">
           <h1 className="text-5xl font-medium mb-6">Welcome InSpace</h1>
           <p className="text-xl mb-12">
-            Un piccolo passo per l'uomo, un grande passo per l'umanita <br></br> oggi ne
-            sono stai fatti due, i viaggi interplanetari sono ora una realtà,
+            Un piccolo passo per l'uomo, un grande passo per l'umanita <br></br>
+            oggi ne sono stai fatti due, i viaggi interplanetari sono ora una
+            realtà,
           </p>
           <Link
             to="/ExplorPlanets"
@@ -22,6 +24,7 @@ const Header = () => {
         </div>
       </section>
     </div>
+    </>
   );
 };
 
--- InSpace/src/components/bundles/header/NavBar.jsx
+++ InSpace/src/components/bundles/header/NavBar.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import HamburgerMenu from "./hamburgerMenu";
-import './navBar.css'
+import "./navBar.css";
 
 const NavBar = () => {
-  const logo = <svg></svg>;
+  const logo = "src/assets/assets-Header/Logo.png";
   <img src={logo} alt="Logo" />;
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -16,31 +16,24 @@ const NavBar = () => {
 
   return (
     <>
-           <div className="wiki-container">
+      <div className="wiki-container">
         <nav className="main-nav">
-          {/* Logo a sinistra */}
-          <div className="logo">
-           <img src={logo} alt="" width='300px' />
-          </div>
           <div className="menu-link">
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Menu</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
+            <div className="logo">
+              <img src={logo} alt="" width="300px" />
+            </div>
+
+            <ul>
+              <Link to="/ExplorPlanets">ExplorPlanets</Link>
+              <Link to="/ExplorPlanets">ExplorPlanets</Link>
+              <Link to="/ExplorPlanets">ExplorPlanets</Link>
+            </ul>
+            <div className="login">
+              <Link to="/ExplorPlanets">Login</Link>
+            </div>
+          </div>
         </nav>
       </div>
-
     </>
   );
 };
