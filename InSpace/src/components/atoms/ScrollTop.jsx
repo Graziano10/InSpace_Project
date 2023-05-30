7aa98e246fa231844c8336afaa3e678c0cdc94a4
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-30 21:07:50 +0200

    scrolltop

--- InSpace/src/components/atoms/ScrollTop.jsx
+++ InSpace/src/components/atoms/ScrollTop.jsx
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
 
 const ScrollTop = () => {
+    const [showButton,setShowButton] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll",() => {
+            if(window.scrollY > 200){
+                setShowButton(true)
+            }
+            else setShowButton(false)
+        })
+    },[])
     const top = () => {
         
            window.scrollTo({
@@ -11,7 +22,7 @@ const ScrollTop = () => {
  
     return (
         <div className="">
-            <img onClick={top} className="w-[110px] -rotate-45 fixed bottom-10 right-0" src="\src\assets\assets-footer\rocket.png" alt="" />
+            <img style={{display: showButton ? 'block': 'none'}} onClick={top} className="w-[110px] -rotate-45 fixed bottom-10 right-0" src="\src\assets\assets-footer\rocket.png" alt="" />
         
         </div>
     )
