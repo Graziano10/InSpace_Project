77bc858c6cf65ac1f37e4d6647660794a0b174bd
antonio l'abbate <antolabb06@gmail.com>
2023-06-08 22:22:42 +0200

    ho cambiato sfondo

--- InSpace/src/pages/wikiComponents/WikiHeader.jsx
+++ InSpace/src/pages/wikiComponents/WikiHeader.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import './Navbar.css'
-
+import video from './WikiAssets/WikiPlanet3.mp4'
+import logo from './WikiAssets/Logo.png'
 import NavBar from "./NavBar2";
 
 
 
 function Navbar() {
     return (
-      <div className="wiki-container bg-Wiki-bg bg-cover bg-center  bg-no-repeat">
+      <div className="wiki-container">
+        <video src={video} autoPlay loop muted></video>
         <NavBar/>
         <header>
-          <h1 className="md:hidden">Ciao sono Lello</h1>
           {/* Contenuto dell'header */}
         </header>
       </div>
