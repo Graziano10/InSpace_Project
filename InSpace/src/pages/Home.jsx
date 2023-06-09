701b947c4fb9484e831791cc3610fc2d3f5349d2
PaoloBaiada <paolosettemaggio@gmail.com>
2023-06-09 15:00:08 +0200

    animations fade

--- InSpace/src/pages/Home.jsx
+++ InSpace/src/pages/Home.jsx
@@ -1,17 +1,19 @@
 import Footer from "../components/bundles/footer/Footer";
 import Header from "../components/bundles/header/Header";
 import Section01 from "../components/bundles/header/Section01";
-import Fade from 'react-reveal'
+
 
 const Home = () => {
   return (
     
       <div>
-        <Fade>
+        <>
+        
         <Header />
         <Section01 />
         <Footer />
-        </Fade>
+        
+        </>
       </div>
     
   );
