346c335eeff5fa6a4997e33e58370fcce334b295
Stefano <maclion@live.com>
2023-06-19 14:36:16 +0200

    second

--- InSpace/src/pages/FormPlanet.jsx
+++ InSpace/src/pages/FormPlanet.jsx
@@ -1,14 +1,26 @@
 // import bg_img from '../assets/formBg.png'
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useRef, useState } from "react";
 import "./formPlanet.css";
 import Fade from "react-reveal/Fade";
 import { Link } from "react-router-dom";
-
+import Footer from "../components/bundles/footer/Footer";
 
 export const FormPlanet = () => {
-  const password_confirm = useRef()
+  const notify = () =>
+    toast.error("Passwords do not match ☄️", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
 
+  const password_confirm = useRef();
   const [data, setData] = useState({
     firstname: "",
     lastname: "",
@@ -18,24 +30,36 @@ export const FormPlanet = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(data.password !== password_confirm.current.value){
-      alert('Passwords do not match!')
-    }else {
-      console.log(data)
+    if (data.password !== password_confirm.current.value) {
+      notify();
+    } else {
+      console.log(data);
     }
   };
 
-  const handleChange = (e)=> {
-    const {name , value} = e.target
-    setData((prevData) => ({...prevData , [name] : value}))
-  }
-
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
   return (
+    
     <section
       id="content"
-      className=" bg-form-bg bg-cover bg-center min-h-screen  flex flex-col xl:flex-row justify-evenly md:items-center xl:justify-end xl:items-start"
+      className=" bg-form-bg bg-cover bg-center  flex flex-col h-full md:items-center  xl:justify-end xl:items-start  xl:flex-row "
     >
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <div className=" backy bg-form-bg8 bg-no-repeat  bg-cover bg-center w-1/2 h-screen  ">
         <Fade>
           <div className="overlay flex justify-center items-center bg-">
@@ -51,31 +75,24 @@ export const FormPlanet = () => {
           </div>
         </Fade>
       </div>
-      <div className=" text-white xl:hidden flex  justify-center items-center p-5 bg-black rounded-none md:bg-transparent ">
-        {/* testi smartphone, tablet */}
-        <div>
-          <h1 className=" text-white text-4xl mb-3 text-center md:text-6xl md:mb-8 italic">
-            Be the next
-          </h1>
-          <p className="text-xl text-center md:text-3xl italic">
-            Universe is waiting for you
-          </p>
-        </div>
-      </div>
-      <Fade>
 
+      <Link
+        to="/"
+        className=" flex justify-center items-center bg-logo h-20  bg-center bg-no-repeat md:w-full xl:hidden"
+      />
+
+      <Fade>
         <form
           action="/register"
           method="POST"
           onSubmit={handleSubmit}
-          className=" shadow-2xl bg-gradient-to-b from-transparent to-blue-700  p-6 md:w-2/3 md:h-min xl:w-1/2 xl:h-screen xl:my-0  rounded-none  xl:bg-gradient-to-b xl:from-orange xl:to-black "
+          className="w-full  shadow-2xl bg-gradient-to-b from-transparent to-blue-950 p-6   xl:w-1/2 xl:h-screen xl:my-0  xl:bg-gradient-to-b xl:from-orange xl:to-black "
         >
-           <Link to="/">
-           <div className="logo w-1/3 h-10 bg-logo bg-cover bg-center xl: m-auto"></div>
-           </Link>
+          <Link to="/">
+            <div className="logo w-1/3 h-10 bg-logo bg-cover bg-center xl: m-auto"></div>
+          </Link>
 
-
-          <div className="grid gap-6 mg-6 md:grid-cols-2 xl:mt-10 ">
+          <div className="grid gap-6 my-6 md:grid-cols-2 xl:mt-10 ">
             <div>
               <label
                 htmlFor="firstname"
@@ -86,7 +103,7 @@ export const FormPlanet = () => {
               <input
                 type="text"
                 id="firstname"
-                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="firstname"
                 required
                 name="firstname"
@@ -94,7 +111,7 @@ export const FormPlanet = () => {
                 onChange={handleChange}
               />
             </div>
-            <div>
+            <div >
               <label
                 htmlFor="last_name"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -104,7 +121,7 @@ export const FormPlanet = () => {
               <input
                 type="text"
                 id="lastname"
-                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="lastname"
                 required
                 name="lastname"
@@ -122,7 +139,7 @@ export const FormPlanet = () => {
               <input
                 type="text"
                 id="company"
-                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Italy , United States, United Kingdom..."
               />
             </div>
@@ -136,7 +153,7 @@ export const FormPlanet = () => {
               <input
                 type="text"
                 id="text"
-                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Address"
               />
             </div>
@@ -150,7 +167,7 @@ export const FormPlanet = () => {
               <input
                 type="tel"
                 id="age"
-                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Phone number"
               />
             </div>
@@ -164,7 +181,7 @@ export const FormPlanet = () => {
               <input
                 type="number"
                 id="age"
-                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Age"
               />
             </div>
@@ -179,7 +196,7 @@ export const FormPlanet = () => {
             <input
               type="email"
               id="email"
-              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="user@gmail.com"
               required
               name="email"
@@ -197,7 +214,7 @@ export const FormPlanet = () => {
             <input
               type="password"
               id="password"
-              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="•••••••••"
               required
               name="password"
@@ -215,12 +232,11 @@ export const FormPlanet = () => {
             <input
               type="password"
               id="confirm_password"
-              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="•••••••••"
               required
               name="password_confirm"
               ref={password_confirm}
-              
             />
           </div>
           <div className="flex items-start mb-6">
@@ -255,7 +271,12 @@ export const FormPlanet = () => {
           </button>
         </form>
       </Fade>
+      <div className="xl:hidden">
+      <Footer/>
+      </div>
     </section>
+    
+    
   );
 };
 
