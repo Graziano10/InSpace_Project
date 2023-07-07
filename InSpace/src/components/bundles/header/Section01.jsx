974bd1adb9d64f8618a0c82e9b63557cf76be47e
Stefano <maclion@live.com>
2023-07-07 14:35:50 +0200

    add: animation to cta button

--- InSpace/src/components/bundles/header/Section01.jsx
+++ InSpace/src/components/bundles/header/Section01.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Fade from 'react-reveal/Fade';
+import "./button.css"
 
 const Section01 = () => {
 
@@ -29,9 +30,17 @@ const Section01 = () => {
             </p>
             <Link
               to="/reservation"
-              className="bg-white text-black py-4 px-12 rounded-[40px] hover:bg-neutral-200 h-9 flex justify-center items-center active:bg-yellow-600"
+              className="bg-white overflow-hidden ctaButton text-black py-4 px-12 rounded-[40px]  h-9 flex justify-center items-center hover:bg-yellow-600 hover:text-white"
             >
-              Make a Reservation
+              RESERVE NOW
+              <div className="arrow-animation ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                </svg>
+              </div>
+
+
+
             </Link>
           </div>
         </section>
