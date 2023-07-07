c06a98a7e99f3b14bc2dc3770f110c39a8e09359
Stefano <maclion@live.com>
2023-07-07 16:30:33 +0200

    add: icons

--- InSpace/src/components/bundles/header/HamburgerMenu.jsx
+++ InSpace/src/components/bundles/header/HamburgerMenu.jsx
@@ -55,16 +55,25 @@ const HamburgerMenu = () => {
                 className="px-4 py-2cursor-pointer mb-10"
               >
                 Explore Planets
+
               </Link>
               <div className="w-full h-[2px] bg-gray-900 mt-2"></div>
             </li>
 
             <li className="px-4 py-2  hover:bg-gray-700 cursor-pointer mb-10">
+
               <Link
                 to="/Login"
                 className="px-4 py-2cursor-pointer mb-10"
               >
-                Login
+                <div className="flex items-center">
+
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                  </svg>
+                  Login
+                </div>
+
               </Link>
               <div className="w-full h-[2px] bg-gray-900  mt-2"></div>
             </li>
